2eef2ba75aea7bb1520c211232177a3c8b1f575b
TVBot <tvbot@example.com>
2025-02-04 10:50:31 -0800

    Fix scope issues with UserPreferencesView

--- Targets/App/Sources/View/Settings/SettingsView.swift
+++ Targets/App/Sources/View/Settings/SettingsView.swift
@@ -11,6 +11,9 @@ import NotifKit
 import SharedKit
 import SwiftUI
 
+// Import local views
+@_exported import UserPreferences
+
 struct SettingsView: View {
 
 	@EnvironmentObject var iap: InAppPurchases
