904a3398cab2e1f296ae0e6f3570892bc0896b60
TVBot <tvbot@example.com>
2025-02-04 11:08:42 -0800

    Add config template and update gitignore

---
+++ Backend/firebase-func/functions/src/config.template.ts
@@ -0,0 +1,18 @@
+//STORE AND EXPORT YOUR API KEYS HERE
+//NOTE: It is generally recommended to store API keys in a more secure way, via something like Google Cloud Secret Manager.
+//This on the other hand is enough for indie devs or smaller projects when there is no risk of malitious team members leaking the keys.
+//Still should be said: DO NOT COMMIT CODE LIKE THIS WITH API KEYS TO PUBLIC REPOS
+
+// NotifKit Keys
+export const osAppApiKey = "YOUR_ONESIGNAL_APP_API_KEY";
+export const osAppID = "YOUR_ONESIGNAL_APP_ID";
+
+// InAppPurchaseKit Keys
+export const rcApiKey = "YOUR_REVENUECAT_API_KEY";
+
+// AnalyticsKit Keys
+export const postHogAPIKey = "YOUR_POSTHOG_API_KEY";
+export const postHogHost = "https://us.i.posthog.com";
+
+// OpenAI (AIKit) Keys
+export const openAIApiKey = "YOUR_OPENAI_API_KEY"; 
